669d91f7bd5e23929cf94a75845cdae82a34c5b2
BT_3W2_07 <simonh8@nycstudents.net>
2023-02-15 09:23:52 -0500

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+        //Testing our random arrays
+        System.out.println("Testing randomIntArr(10)");
+        Integer[] testIntAr1 = SortUtils.randomIntArr(10);
+        for (Integer val: testIntAr1)
+        {
+            System.out.print(val+" ");
+        }
+        System.out.println();
+        System.out.println("Testing randomStrArr(10)");
+        String[] testStrAr1 = SortUtils.randomStrArr(10);
+        for (String val: testStrAr1)
+        {
+            System.out.print(val+" ");
+        }
+        System.out.println();
+        System.out.println("Testing randomIntAL\n"+SortUtils.randomIntAL(10));
+        System.out.println("Testing randomStrAL\n"+SortUtils.randomStrAL(10));
+        System.out.println();
+
+        ArrayList<Integer> test = SortUtils.randomIntAL(6);
+        System.out.println(test);
+        System.out.println(SortUtils.minIndex(3, test));
+        SortUtils.swap(2, 5, test);
+        System.out.println(test);
+        System.out.println();
+
+        ArrayList<String> test2 = SortUtils.randomStrAL(6);
+        System.out.println(test2);
+        System.out.println(SortUtils.minIndex(0, test2));
+        SortUtils.swap(0, 5, test2);
+        System.out.println(test2);
+    }
 }
--- src/SortUtils.java
+++ src/SortUtils.java
@@ -1,2 +1,85 @@
-public class SortUtils {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+/** Static methods for aiding in the creation of sorting algorithms */
+public class SortUtils
+{
+    /** Add static int that counts memory accesses*/
+    private static int memAccess = 0;
+
+    /** Add static int that counts comparisons */
+    private static int comparisons = 0;
+    /** Add public static getters and setters for both 
+
+     /** Checks if an array of integers is non-descending order */
+    public static boolean isSorted(ArrayList<Integer> intArr)
+    {
+        return false;
+    }
+    public static int getMemAccess() {
+        return memAccess;
+    }
+    public static void setMemAccess(int memAccess) {
+        SortUtils.memAccess = memAccess;
+    }
+    public static int getComparisons() {
+        return comparisons;
+    }
+    public static void setComparisons(int comparisons) {
+        SortUtils.comparisons = comparisons;
+    }
+    /** Checks if an array of String is non-descending order */
+    public static boolean isSortedStr(String[] strArr)
+    {
+        return false;
+    }
+    /** Creates a random Array of Integers of a given length */
+    public static Integer[] randomIntArr(int len)
+    {
+        //Keeping things simple 0 to 99
+        Integer[] retArr = new Integer[len];
+        for (int i = 0; i < len; i++)
+        {
+            retArr[i] = (int) (Math.random()*100);
+        }
+        return retArr;
+    }
+    /** Creates a random ArrayList of Strings of a given length */
+    public static String[] randomStrArr(int len)
+    {
+        String letters = "abcdefghijklmnopqrstuvwxyz";
+        //Keeping things simple a to z
+        String[] retArr = new String[len];
+        for (int i = 0; i < len; i++)
+        {
+            int randomInd = (int) (Math.random()*26);
+            retArr[i] = letters.substring(randomInd,randomInd+1);
+        }
+        return retArr;
+    }
+    /** Creates a random ArrayList of Integers of a given length */
+    public static ArrayList<Integer> randomIntAL(int len)
+    {
+        return new ArrayList<Integer>(Arrays.asList(randomIntArr(len)));
+    }
+    /** Creates a random ArrayList of Integers of a given length */
+    public static ArrayList<String> randomStrAL(int len)
+    {
+        return new ArrayList<String>(Arrays.asList(randomStrArr(len)));
+    }
+    /** Finds the minimum starting at index i **/
+     public static int minIndex(int i, ArrayList arrayList){
+        int minIdx = i;
+        for (int j = i; j < arrayList.size(); j++){
+            if (String.valueOf(arrayList.get(j)).compareTo(String.valueOf(arrayList.get(minIdx))) < 0){
+                minIdx = j;
+            }
+        }
+        return minIdx;
+    }
+    /** Swaps the elements at index i and j **/
+    public static void swap(int i, int j, ArrayList arrayList){
+        String old = String.valueOf(arrayList.set(i, arrayList.get(j)));
+        arrayList.set(j, old);
+    }
 }
